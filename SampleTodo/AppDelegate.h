d0156501b61194ab2832002245d5cf1643f25844
Damian Martinez <damian-git@malachiarts.com>
2015-02-11 11:44:54 -0800

    Initial Commit

---
+++ SampleTodo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SampleTodo
+//
+//  Created by d on 11/2/15.
+//  Copyright (c) 2015 d. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SampleTodo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SampleTodo
+//
+//  Created by d on 11/2/15.
+//  Copyright (c) 2015 d. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
